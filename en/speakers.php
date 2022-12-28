5050f16160caf652ad0029d43e7d349aac89e7b4
Max Base <maxbasecode@gmail.com>
2022-12-28 14:34:21 +0100

    update speacker

--- en/speakers.php
+++ en/speakers.php
@@ -7,7 +7,7 @@ $en = true;
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Speakers - Fourth Conference on Computational Algebra, Computational Number Theory and Applications</title>
+        <title>Keynote Speakers - Fourth Conference on Computational Algebra, Computational Number Theory and Applications</title>
         <!-- Fonts -->
         <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/sahel-font@v3.4.0/dist/font-face.css">
         <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css">
@@ -15,30 +15,30 @@ $en = true;
         <link rel="stylesheet" href="/style.css?ver=1">
     </head>
     <body>
-        <?php include "_header.php" ?>
+        <?php include "_inc/_header.php" ?>
         
         <section class="normal">
-            <h2>سخنرانان کلیدی</h2>
+            <h2>Keynote Speakers</h2>
 
             <p>
-                این لیست در حال بروزرسانی است، در صورت تمایل جهت ارائه سخنرانی، درخواست خود را به ایمیل کمیته علمی کنفرانس ارسال کنید.
+                This list is being updated, if you want to give a talk, please send your request to the email address of the scientific committee.
             </p>
 
             <ul class="speakers">
                 <li>
                     <img src="/دقیق.jpg" alt="Dr. Hasan Daghigh">
-                    <h4 class="speaker-name">دکتر حسن دقیق</h4>
-                    <h5 class="speaker-university">دانشگاه کاشان</h5>
+                    <h4 class="speaker-name">Hasan Daghigh</h4>
+                    <h5 class="speaker-university">University of Kashan</h5>
                 </li>
                 <li>
                     <img src="/فتح تبار.jpg" alt="Gholamhosein Fathtabar">
-                    <h4 class="speaker-name">دکتر غلامحسین فتح تبار</h4>
-                    <h5 class="speaker-university">دانشگاه کاشان</h5>
+                    <h4 class="speaker-name">Gholamhosein Fathtabar</h4>
+                    <h5 class="speaker-university">University of Kashan</h5>
                 </li>
             </ul>
         </section>
 
-        <?php include "_footer.php" ?>
+        <?php include "_inc/_footer.php" ?>
 
         <script type="text/javascript" src="/script.js?ver=1"></script>
     </body>
