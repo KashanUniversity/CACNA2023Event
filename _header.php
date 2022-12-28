b388538d31137de1a035b7d5d4fb0025d48669a0
Max Base <maxbasecode@gmail.com>
2022-12-28 13:58:03 +0100

    update style

--- _header.php
+++ _header.php
@@ -1,3 +1,4 @@
+<!-- main header/slider -->
 <section class="slider<?= !isset($is_home) ? " mini-slider" : "" ?>">
     <img src="/logo.png" alt="دانشگاه کاشان">
     <h1>
@@ -11,3 +12,15 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm0 18c4.42 0 8-3.58 8-8s-3.58-8-8-8-8 3.58-8 8 3.58 8 8 8zm1-8h3l-4 4-4-4h3V8h2v4z" fill="rgba(255,255,255,1)"/></svg>
     </div>
 </section>
+
+<!-- nav -->
+<nav>
+    <li><a href="/">صفحه‌ی اصلی</a></li>
+    <li><a href="/speakers.php">سخنرانان</a></li>
+    <li><a href="/scientific-committee.php">کمیته علمی</a></li>
+    <li><a href="/executive-committee.php">کمیته اجرایی</a></li>
+    <li><a href="/registration.php">ثبت‌نام</a></li>
+    <li><a href="/submit.php">ارسال مقاله</a></li>
+    <!-- <li><a href="/schedule.php">برنامه</a></li> -->
+    <!-- <li><a href="/contact.php">تماس با ما</a></li> -->
+</nav>
