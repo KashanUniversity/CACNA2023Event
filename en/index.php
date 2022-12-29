77a1f7b7694321808414d877bcb02524bbcc8c1b
Max Base <maxbasecode@gmail.com>
2022-12-29 11:38:22 +0100

    adding meta tags

--- en/index.php
+++ en/index.php
@@ -9,6 +9,9 @@ $is_home = true;
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Fourth Conference on Computational Algebra, Computational Number Theory and Applications</title>
+        <!-- meta tags -->
+        <meta name="description" content="We are very happy to announce the holding of the 4th Conference on Computational Algebra, Computational Theory of Numbers and Applications on 24th to 26th March 2023. The first, second and third conferences of this series were held in Kashan University in the years 2013 (26 to 28 December), 2014 (21 to 23 October) and 2017 (21 to 23 December), and this is the fourth conference of this series. This conference covers all topics of computational algebra, computer algebra, computational number theory and their applications. The lectures of this seminar are virtual and those who are interested are invited to send their articles for this conference.">
+        <meta name="keywords" content="Conference, Event, Talks, Computational theory of groups, Computational displacement algebra, Computational theory of rings, Computational theory of numbers, Coding theory, Algebraic Machine Learning, Complex networks, Computational algebraic combinations, Computational linear algebra, Computing Combinations">
         <!-- Fonts -->
         <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/sahel-font@v3.4.0/dist/font-face.css">
         <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css">
