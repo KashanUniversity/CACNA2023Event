e5336b4edf02a77c26d445130e3ebca4dd9923b7
Max Base <maxbasecode@gmail.com>
2023-01-03 08:16:00 +0100

    update text

--- en/index.php
+++ en/index.php
@@ -30,13 +30,12 @@ $is_home = true;
                     <img class="cover" src="/poster-en.jpg" alt="Fourth Conference on Computational Algebra, Computational Number Theory and Applications">
                 </a>
 
-                <b>Holding the 4th Conference on Computational Algebra, Computational Theory of Numbers and Applications on 24th to 26th March 2023</b>
+                <b>Holding the 4th Conference on Computational Algebra, Computational Theory of Numbers and Applications on 15th to 17th March 2023</b>
                 <br>
-                We are very happy to announce the 4th Conference on Computational Algebra, Computational Number Theory  and Applications (CACNA 2023) wil be held at the University of Kashan, on  March 15 - 17, 2023.
+                We are very happy to announce the 4th Conference on Computational Algebra, Computational Number Theory and Applications (CACNA 2023) wil be held at the University of Kashan, on  March 15 - 17, 2023.
                 <br>
                 <br>
-                The first, second and third conferences of this series were held at the University of Kashan during the years 2013 (26 to 28 December), 2014 (21 to 23 October) and 2017 (21 to 23 December), and this is the fourth conference of this series. This conference covers all topics of computational algebra, computational number theory and their applications.
-                All lectures of this conference will be virtual and those who are interested are invited to send their articles for presenting on this conference.
+                The first, second and third conferences of this series were held at the University of Kashan during the years 2013 (26 to 28 December), 2014 (21 to 23 October) and 2017 (21 to 23 December), and this is the fourth conference of this series. This conference covers all topics of computational algebra, computational number theory and their applications. All  lectures of this conference will be virtual and those who are interested are invited to send their articles for presenting on this conference.
                 <br>
                 <br>
                 Ali Reza Ashrafi,
