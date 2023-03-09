66ed45acb66e872489f4ed98321adbf5b195e464
Max Base <maxbasecode@gmail.com>
2023-03-09 17:25:14 +0000

    update header

--- en/_inc/_header.php
+++ en/_inc/_header.php
@@ -4,9 +4,10 @@
     <h1>
         <span>4th</span>
         Conference on Computational Algebra, Computational Number Theory and Applications
+        (Memorial of Professor Alireza Ashrafi)
     </h1>
     <h2>
-        March 15 - 17, 2023
+        July 5 - 7, 2023
     </h2>
     <div class="arrow-icon">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm0 18c4.42 0 8-3.58 8-8s-3.58-8-8-8-8 3.58-8 8 3.58 8 8 8zm1-8h3l-4 4-4-4h3V8h2v4z" fill="rgba(255,255,255,1)"/></svg>
