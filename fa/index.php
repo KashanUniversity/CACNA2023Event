3062a979474ffb49bfa66a822339188523eba758
Max Base <MaxBaseCode@Gmail.Com>
2023-07-02 17:18:02 +0100

    Update index.php

--- fa/index.php
+++ fa/index.php
@@ -23,13 +23,30 @@ $is_home = true;
         <section class="normal" style="padding-bottom: 0px;">
             <h2>برنامه زمانی جلسه و ارائه های کنفرانس</h2>
             <p>
-                <a href="program.php">
+<!--                 <a href="program.php">
                     <font color="red">
                         جهت مشاهده جدول برنامه زمانی کنفرانس اینجا کلیک کنید.
                     </font>
                 </a>
+            </p> -->
+                <a href="/4july.pdf">
+                    <font color="red">
+                        مشاهده برنامه زمانی سه شنبه 13ام تیرماه 1402
+                    </font>
+                </a>
+                <br>
+                <a href="/5july.pdf">
+                    <font color="red">
+                        مشاهده برنامه زمانی چهار شنبه 14ام تیرماه 1402
+                    </font>
+                </a>
+                <br>
+                <a href="/6july.pdf">
+                    <font color="red">
+                        مشاهده برنامه زمانی پنج شنبه 15ام تیرماه 1402
+                    </font>
+                </a>
             </p>
-            <br>
 
             <h2>قابل توجه کلیه شرکت کنندگان محترم کنفرانس</h2>
             <p>
