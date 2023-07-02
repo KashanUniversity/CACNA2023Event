46d78c87d5f77480c3cceec379c64d58e84eeb9c
Max Base <MaxBaseCode@Gmail.Com>
2023-07-02 06:40:56 +0100

    Update _submit_file.php

--- _submit_file.php
+++ _submit_file.php
@@ -18,16 +18,16 @@ if (isset($_POST["submit"], $_POST["title"], $_POST["title_en"], $_POST["author"
     if ($poster !== null && ($poster['error'] == 4 || ($poster['size'] == 0 && $poster['error'] == 0))) $poster = null;        
 
     // Save file in papers directory with a random filename
-    if (!file_exists("../videos")) mkdir("../videos");
+    if (!file_exists("../newvideos")) mkdir("../newvideos");
     if ($video !== null) {
         $file_name = uniqid() . uniqid() . $video["name"];
-        move_uploaded_file($video["tmp_name"], "../videos/$file_name");
+        move_uploaded_file($video["tmp_name"], "../newvideos/$file_name");
     }
 
-    if (!file_exists("../posters")) mkdir("../posters");
+    if (!file_exists("../newposters")) mkdir("../newposters");
     if ($poster !== null) {
         $file_name = uniqid() . uniqid() . $poster["name"];
-        move_uploaded_file($poster["tmp_name"], "../posters/$file_name");
+        move_uploaded_file($poster["tmp_name"], "../newposters/$file_name");
     }
 
     $obj = [
@@ -39,20 +39,20 @@ if (isset($_POST["submit"], $_POST["title"], $_POST["title_en"], $_POST["author"
         "file" => $file_name,
     ];
     if ($video !== null) {
-        $obj["link"] = "https://cacna2023.kashanu.ac.ir/videos/" . $file_name;
+        $obj["link"] = "https://cacna2023.kashanu.ac.ir/newvideos/" . $file_name;
     } else if ($poster !== null) {
-        $obj["link"] = "https://cacna2023.kashanu.ac.ir/posters/" . $file_name;
+        $obj["link"] = "https://cacna2023.kashanu.ac.ir/newposters/" . $file_name;
     }
     $obj["video"] = $video;
     $obj["poster"] = $poster;
 
     // Write and create utf8 file
     if ($video !== null) {
-        file_put_contents("../_secure_videos.json", json_encode($obj, JSON_UNESCAPED_UNICODE) . "\n==============================\n", FILE_APPEND);
+        file_put_contents("../_secure_newvideos.json", json_encode($obj, JSON_UNESCAPED_UNICODE) . "\n==============================\n", FILE_APPEND);
     } else if ($poster !== null) {
-        file_put_contents("../_secure_posters.json", json_encode($obj, JSON_UNESCAPED_UNICODE) . "\n==============================\n", FILE_APPEND);
+        file_put_contents("../_secure_newposters.json", json_encode($obj, JSON_UNESCAPED_UNICODE) . "\n==============================\n", FILE_APPEND);
     } else {
-        file_put_contents("../_secure_file.json", json_encode($obj, JSON_UNESCAPED_UNICODE) . "\n==============================\n", FILE_APPEND);
+        file_put_contents("../_secure_newfiles.json", json_encode($obj, JSON_UNESCAPED_UNICODE) . "\n==============================\n", FILE_APPEND);
     }
 
     $sent = true;
