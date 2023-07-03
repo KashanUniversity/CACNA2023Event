0e6121953eee3dd29149f84728e74da43fd022fa
Max Base <MaxBaseCode@Gmail.Com>
2023-07-03 09:28:46 +0100

    Create program.php

---
+++ fa/program.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="fa_IR" dir="rtl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>اطلاع رسانی ها - چهارمین کنفرانس جبر محاسباتی، نظریه‌ی محاسباتی اعداد و کاربردها (یادبود پروفسور علی‌رضا اشرفی)</title>
+        <!-- meta tags -->
+        <meta name="description" content="بسیار خوشحالیم که برگزاری چهارمین همایش جبر محاسباتی، نظریه‌ی محاسباتی اعداد و کاربردها را در تاریخ ۱۳ الی ۱۵ تیر ماه ۱۴۰۲ به اطلاع علاقه‌مندان برسانیم. همایش‌های اول، دوم و سوم از این سری در سال‌های  ۱۳۹۳ (۲۶ الی ۲۸ آذر)،  ۱۳۹۴ (۲۱ الی ۲۳ مهر ماه) و ۱۳۹۷ (۲۱ الی ۲۳ آذر ماه)  در دانشگاه کاشان برگزار شده و این چهارمین همایش از این مجموعه است. این همایش تمامی مباحث جبر محاسباتی، جبر کامپیوتری، نظریه‌ی محاسباتی اعداد و کاربردهای آن‌ها را پوشش می‌دهد. سخنرانی‌های این سمینار به صورت مجازی بوده و از علاقه‌مندان دعوت می‌شود مقالات خود را برای این همایش ارسال کنند.">
+        <meta name="keywords" content="کنفرانس، رویداد، سخنرانی، نظریه محاسباتی گروه ها، جبر جابجایی محاسباتی، نظریه محاسباتی حلقه ها، نظریه محاسباتی اعداد، کد و رمز، یادگیری ماشین جبری، شبکه های پیچیده، ترکیبیات جبری محاسباتی، جبر خطی محاسباتی، ترکیبیات محاسباتی">
+        <!-- Fonts -->
+        <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/sahel-font@v3.4.0/dist/font-face.css">
+        <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css">
+        <!-- Style -->
+        <link rel="stylesheet" href="/style.css?ver=1">
+    </head>
+    <body>
+        <?php include "_inc/_header.php" ?>
+
+        <!-- <section class="normal" style="margin-bottom: 0px;">
+            <h2>اسامی مقاله های پذیرفته شده کنفرانس</h2>
+            <p>
+                جهت دانلود اسامی مقاله های پذیرش شده می توانید سه فایل زیر که به فرمت اکسل هستند را دانلود و بررسی نمایید:
+                <br>
+                <a href="/aa.xlsx">قسمت اول</a>
+                    <br>
+                <a href="/bb.xlsx">قسمت دوم</a>
+                    <br>
+                <a href="/cc.xlsx">قسمت سوم</a>
+            </p>
+        </section> -->
+
+        <section class="normal">
+            <h2>قابل توجه کلیه شرکت کنندگان محترم کنفرانس</h2>
+            <p style="text-align: justify;">
+
+              <table width="100%" border="1">
+                <thead>
+                <tr>
+                  <td colspan="2">سه شنبه صبح</td>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Opening</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/i0v8l3j9vvglswsno4vp</td>
+                  </tr>
+
+
+                                  <tr>
+                    <td>Tuesday-Agebra (A)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/z4qlq3wph9l5ehaaglt8</td>
+                  </tr>
+
+                                  <tr>
+                    <td>Tuesday-Algebra(B)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/fkk2mubxqufvxx9d55iy</td>
+                  </tr>
+
+                                  <tr>
+                    <td>Tuesday-Invited Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/jkq40ln3fpqcn13pkmg4</td>
+                  </tr>
+
+                                  <tr>
+                    <td>Tuesday-Graph(A)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/971fvr1f08s4it8i2q2v</td>
+                  </tr>
+
+                                  <tr>
+                    <td>Tuesday-Numerical Analysis</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/ruts3xu5kjvw8z1aggaf</td>
+                  </tr>
+
+</tbody>
+              </table>
+
+
+		<br><br>
+
+
+              <table width="100%" border="1">
+                <thead>
+                <tr>
+                  <td colspan="2">سه شنبه عصر</td>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Tuesday-(Herzog)_Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/2lnn6pu80stphq87y0gm</td>
+                  </tr>
+
+
+                  <tr>
+                    <td>Tuesday-Algebra(A)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/a7mdffn1n3nmanl9dfaw</td>
+                  </tr>
+
+
+
+
+                  <tr>
+                    <td>Tuesday-Algebra(B)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/c8wbm1vgqyv6w7tfo04e</td>
+                  </tr>
+
+
+                  <tr>
+                    <td>Tuesday-Keynote Speaker Graph and Computer Science</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/8940zb0h272ckyv43n08</td>
+                  </tr>
+
+
+                  <tr>
+                    <td>Tuesday-Graph(A)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/bl1obdxjs9y3p2o5i2y2</td>
+                  </tr>
+
+
+
+                  <tr>
+                    <td>Tuesday-(P. Stanimirovic)_Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/5my4tek13ujccptd1rlg</td>
+                  </tr>
+
+
+
+
+
+                  <tr>
+                    <td>Tuesday-(M. Javarsineh)_Invited Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/oirav1n5u66tcaowwpml</td>
+                  </tr>
+
+
+
+
+                  <tr>
+                    <td>Tuesday-Graph(B)</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/cvgrjk4tqj1q1yera0wz</td>
+                  </tr>
+
+
+                  <tr>
+                    <td>Tuesday-Optimization</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/nl7adw9d56vbiemhrcgc</td>
+                  </tr>
+
+
+                  <tr>
+                    <td>Tuesday-(Jalili)_Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/xk8fwtn5rbq5k533r110</td>
+                  </tr>
+
+
+                  <tr>
+                    <td>Tuesday_Computer Science</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/djmr0sb8lxzshxsme70j</td>
+                  </tr>
+                </tbody>
+              </table>
+
+
+		<br><br>
+
+
+
+              <table width="100%" border="1">
+                <thead>
+                <tr>
+                  <td colspan="2">چهارشنبه صبح</td>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Wednesday-Algebra</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/uo8ozet5u7dxb3ua03oi</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday-Graph and Computer Science</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/iyg6yi269pjhvovv3751</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday-Graph</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/6ct5o8leh64x2yp6wkyc</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday-(Alain Bretto)-Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/x2cl1osko01hit8q3r3n</td>
+                  </tr>
+                </tbody>
+              </table>
+
+
+          		<br><br>
+
+
+              <table width="100%" border="1">
+                <thead>
+                <tr>
+                  <td colspan="2">چهارشنبه عصر</td>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Wednesday-(M. Ghandhari)_Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/bnf4wlvqqk754yp0r0a6</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday_Graph</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/u0hl9hurxr7ayas3hwfn</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday_Algebra</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/hq721g1aid0x8vol5081</td>
+                  </tr>
+                  <tr>
+                    <td>Poster</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/7174mosiiywfjpamzzr2</td>
+                  </tr>
+                </tbody>
+              </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+          		<br><br>
+
+
+              <table width="100%" border="1">
+                <thead>
+                <tr>
+                  <td colspan="2">پنج شنبه صبح</td>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Thursday-(Abdollahi)_Keynote talk</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/fqn1g9yis09bcdaj0hom</td>
+                  </tr>
+                  <tr>
+                    <td>Group-Graph</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/9lb25acp7wexx1zq4efi</td>
+                  </tr>
+                </tbody>
+              </table>
+
+
+
+
+          		<br><br>
+
+
+              <table width="100%" border="1">
+                <thead>
+                <tr>
+                  <td colspan="2">پنج شنبه عصر</td>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Thursday-(Mosavi)_Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/06oh40zwnu998juap9ar</td>
+                  </tr>
+                  <tr>
+                    <td>Thursday_Group</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/ygzjef4k50h0079ll9zv</td>
+                  </tr>
+
+                
+                
+                                  <tr>
+                    <td>Thursday-(Alikhani)_Keynote Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/leqwehfx9nabxras0vsz</td>
+                  </tr>
+                  <tr>
+                    <td>Thursday Graph</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/op2ex2rdqy9g2alz18ft</td>
+                  </tr>
+                  <tr>
+                    <td>Thursday-(Tavakoli)_Invited Speaker</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/dm915vl77g5l2yshykax</td>
+                  </tr>
+                  <tr>
+                    <td>Closing Ceremony</td>
+                    <td>https://meeting.kashanu.ac.ir/guest-join/xstg290g519jqrb52v6j</td>
+                  </tr>
+</tbody>
+              </table>
+
+
+            </p>
+        </section>
+
+        <?php include "_inc/_footer.php" ?>
+
+        <script src="/script.js?ver=1"></script>
+    </body>
+</html>
