8ef27777c913cfceeb0aaf88ba7f79c3b54470b0
Max Base <MaxBaseCode@Gmail.Com>
2023-07-05 08:48:21 +0100

    Update posters.php

--- fa/posters.php
+++ fa/posters.php
@@ -1,3 +1,40 @@
+<?php
+$posters_data = 'اکرامی	سید خلیل 	جبر	A fixed point Approach to Almost Orthogonally Quadratic	https://video.kashanu.ac.ir/cacna2023/posters/649c2704b4c0c649c2704b4c0eEkrami - A Fixed Point Approach to Almost Orthogonally Quadratic star-Homomorphisms.mp4
+اکرامی	سید خلیل 	جبر	Almost Hilbert C∗-Module Derivations	https://video.kashanu.ac.ir/cacna2023/posters/649b505138e08649b505138e09Ekrami - Almost Hilbert Cstar-Module derivations.mp4
+پویان	ندا	جبر	on unit-semi boolean rings	https://video.kashanu.ac.ir/cacna2023/posters/649bf3105996b649bf3105996cunit-semi-boolean-poster-neda pouyan.mp4
+جمالی	مرضیه	جبر	Study of classical prime submodules	https://video.kashanu.ac.ir/cacna2023/posters/6499c3a77209d6499c3a77209eposter jamali.mp4
+حسن زاده	امید	جبر	A note on UJI rings	https://video.kashanu.ac.ir/cacna2023/posters/649be9a015b24649be9a015b26فایل ارایه پوستر.mp4
+دارابی	حمید	جبر	Capability of low dimensional nilpotent 3-Lie algebras	https://video.kashanu.ac.ir/cacna2023/newposters/64a28b4a52bf264a28b4a52bf4دارابی.pptx
+رمضانی	سید مهراب	جبر	Solving functional equations with the help of representation of finite groups	https://video.kashanu.ac.ir/cacna2023/posters/649d5104d422f649d5104d4232video_2023-06-25_19-35-07.mp4
+سریع القلم	نرگس	جبر	Algebra and Uncertainty Business;  A missing ring in productive business Management- Applications and Implications	https://cacna2023.kashanu.ac.ir/newposters/64a29271f1f9864a29271f1f9aسریع القلم.mp4
+کیانی	سیما	جبر	Roman domination number in the unitary Cayley graphs	https://video.kashanu.ac.ir/cacna2023/posters/649dbd62086ba649dbd62086bcSima Kiani2 poster.mp4
+مسعودی آرانی	مریم	جبر	A study on weakly reversible rings	https://video.kashanu.ac.ir/cacna2023/posters/6499aec3a3b3c6499aec3a3b3fMasoudi Poster.mp4
+جعفری پطرودی	سید حسین	جبر	On Particular Number Sequences, Some Properties and New Results	https://cacna2023.kashanu.ac.ir/newposters/64a2ca9bdddcb64a2ca9bdddcdjafaripetroudi.mp4
+رحمتی	مرضیه	جبر	intermediate axiom on algebra structure	https://video.kashanu.ac.ir/cacna2023/newposters/64a28279034ac64a28279034aeRahmati-poster2.mp4
+یعقوبی	مریم	جبر	some properties of L-Hutton uniform spaces ,L-fuzzy	https://video.kashanu.ac.ir/cacna2023/posters/649dba56d55f3649dba56d55f4M-Yaghoubi1.mp4
+PARVEEN	P	Graph	CERTAIN PROPERTIES OF THE ENHANCED POWER GRAPH ASSOCIATEDWITH A FINITE GROUP	https://cacna2023.kashanu.ac.ir/newposters/64a2dc0bbad1d64a2dc0bbad1eCertain_properties_of_the_enhanced_power_graph_associated_with_a_finite_group__Poster.pdf
+رباط جزی	اکرم	گراف	محاسبه عدد رنگی مکانی - همسایه ای گراف های فولرن و پترسن	https://video.kashanu.ac.ir/cacna2023/posters/64994e77197a664994e77197a9Poster(1) (5).pptx
+سبزواری	مهدی	گراف	کاربردهایی از قدم زدن تصادفی روی گراف ها	https://video.kashanu.ac.ir/cacna2023/newposters/64a2857b8803964a2857b8803bsabzevari 1.pptx
+شاه میری	زینب	گراف	بعد متریک ‑kخوشه غیرمحلی کسری گراف ها	https://video.kashanu.ac.ir/cacna2023/newposters/64a27ddb28f8664a27ddb28f87فیلم پوستر(شاه میری&توکلی).mp4
+شرفدینی	رضا	گراف	On neighbourhood Laplacian matrix of graphs	https://video.kashanu.ac.ir/cacna2023/newposters/64a13609b063964a13609b063bsharafdini-poster.mp4
+عقدایی	ابوالفضل	گراف	PTn یافتن عدد وینر برای گراف پلی تیوفتن و ارائه یک چند جمله ای درجه سه با استفاده از عدد	https://video.kashanu.ac.ir/cacna2023/posters/649b0ac016fb1649b0ac016fb3A-Aghdaee.mp4
+گلابی سنجانی	انسیه	گراف	اثر حاصلضرب توانهای ماتریس مجاورت و ماتریس درجه	https://video.kashanu.ac.ir/cacna2023/posters/649b3bbc5c7a4649b3bbc5c7a5Rec 0004.mp4
+میروکیلی 	سعید	گراف	On the soft coloring of soft graph	https://video.kashanu.ac.ir/cacna2023/posters/649da9522d049649da9522d04bPoster-Mirvakili.mp4
+سلطانی علاسوند	مهرنوش	آنالیز عددی	روش حلی برای مسائل برنامه ریزی خطی کسری بازه ای	https://video.kashanu.ac.ir/cacna2023/posters/649b42d995737649b42d995738فایل ارائه پوستر-مهرنوش سلطانی علاسوند.mp4
+سبزواری	مهدی	آنالیز عددی	استفاده از موجک های CASدر حل عددی معادلات انتگرال فردهلم فازی غیرخطی نوع دوم	https://video.kashanu.ac.ir/cacna2023/newposters/64a284233d88b64a284233d88dsabzevari 2.pptx
+ذبیحی	فاطمه	آنالیز عددی	A new computational method based on Vieta-Lucas	https://video.kashanu.ac.ir/cacna2023/posters/649add1c4e7e9649add1c4e7ebzabihi.mp4
+ذبیحی	فاطمه	آنالیز عددی	"Legendre wavelet operational scheme for solving the model of pollution for a system of lakes	https://cacna2023.kashanu.ac.ir/newposters/64a3f044bb6c564a3f044bb6c7zabihi-pullation.mp4
+راه پیمایی	فرزاد	آنالیز عددی	یک روش بارزیلای-بوروین اصلاح شده برای حل معادلات قدر مطلقی	https://video.kashanu.ac.ir/cacna2023/newposters/64a1e2c07e07364a1e2c07e075Screenrecorder-2023-07-02-23-52-37-158.mp4
+تبریزی دوز	حمیدرضا	آنالیز عددی	معرفی یک پایه درروش گالرکین براساس پایه موجک چندگانه لژاندر	https://cacna2023.kashanu.ac.ir/newposters/64a32db5661a364a32db5661a5فیلم پوستر تبریزی دوز.mp4
+عسگری	جلال	بهینه سازی	he improved bounds for the Seidel and Laplacian Seidel energy	https://cacna2023.kashanu.ac.ir/newposters/64a2a3682900564a2a36829007cacna2023-poster-en-askari.pptx
+سلطانی	زینب	بهینه سازی	On a non-compact Minimax Theorem	https://video.kashanu.ac.ir/cacna2023/posters/649aa13a9dca7649aa13a9dca9زینب سلطانی (2).mp4
+بای	عاطفه	بهینه سازی	یک روش گرادیان مزدوج اصلاح شده برای حل مسائل بهینه سازی ناهموار در بازسازی تصویر	https://video.kashanu.ac.ir/cacna2023/newposters/64a207b214eb064a207b214eb2vcompress_3.mov 
+نامی	محمد مهدی	آموزش ریاضی	مزایای استفاده از هوش مصنوعی در آموزش ریاضیات	https://cacna2023.kashanu.ac.ir/newposters/64a30c51af0c664a30c51af0c8Rec 0003.mp4
+اکبری	فاطمه	بهینه سازی	بررسی تدوین ابزار بهره وری در تحقیق در عملیات (مطالعه موردی شرکت های موفق)	
+حسن خانی	محمد	گراف	A note on the specctrum of a graph';
+$posters = explode("\n", $posters_data);
+foreach ($posters as $i => $poster_line) $posters[$i] = expllode("\t", $poster_line);
+?>
 <!DOCTYPE html>
 <html lang="fa_IR" dir="rtl">
     <head>
@@ -19,8 +56,28 @@
 
         <section class="normal" style="margin-bottom: 0px;">
             <h2>اسامی مقاله های پذیرفته شده پوستری کنفرانس</h2>
-            <p>
-            </p>
+            <table width="100%" border="2">
+                <thead>
+                    <tr>
+                        <td>نام و نام خانوادگی</td>
+                        <td>رشته</td>
+                        <td>عنوان مقاله</td>
+                        <td>فایل ارائه</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($posters as $poster_item) { ?>
+                    <tr>
+                        <td><?= $poster_item[0] ?> <?= $poster_item[1] ?></td>
+                        <td><?= $poster_item[2] ?></td>
+                        <td><?= $poster_item[3] ?></td>
+                        <td>
+                            <a href="<?= $poster_item[4] ?>">دانلود</a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         </section>
 
         <?php include "_inc/_footer.php" ?>
