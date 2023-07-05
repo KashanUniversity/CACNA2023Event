d0a6894f467cf0ac18ce451a243bead6dd0637a6
Max Base <MaxBaseCode@Gmail.Com>
2023-07-05 08:40:36 +0100

    Create posters.php

---
+++ fa/posters.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fa_IR" dir="rtl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>پوستر‌هاا - چهارمین کنفرانس جبر محاسباتی، نظریه‌ی محاسباتی اعداد و کاربردها (یادبود پروفسور علی‌رضا اشرفی)</title>
+        <!-- meta tags -->
+        <meta name="description" content="بسیار خوشحالیم که برگزاری چهارمین همایش جبر محاسباتی، نظریه‌ی محاسباتی اعداد و کاربردها را در تاریخ ۱۳ الی ۱۵ تیر ماه ۱۴۰۲ به اطلاع علاقه‌مندان برسانیم. همایش‌های اول، دوم و سوم از این سری در سال‌های  ۱۳۹۳ (۲۶ الی ۲۸ آذر)،  ۱۳۹۴ (۲۱ الی ۲۳ مهر ماه) و ۱۳۹۷ (۲۱ الی ۲۳ آذر ماه)  در دانشگاه کاشان برگزار شده و این چهارمین همایش از این مجموعه است. این همایش تمامی مباحث جبر محاسباتی، جبر کامپیوتری، نظریه‌ی محاسباتی اعداد و کاربردهای آن‌ها را پوشش می‌دهد. سخنرانی‌های این سمینار به صورت مجازی بوده و از علاقه‌مندان دعوت می‌شود مقالات خود را برای این همایش ارسال کنند.">
+        <meta name="keywords" content="کنفرانس، رویداد، سخنرانی، نظریه محاسباتی گروه ها، جبر جابجایی محاسباتی، نظریه محاسباتی حلقه ها، نظریه محاسباتی اعداد، کد و رمز، یادگیری ماشین جبری، شبکه های پیچیده، ترکیبیات جبری محاسباتی، جبر خطی محاسباتی، ترکیبیات محاسباتی">
+        <!-- Fonts -->
+        <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/sahel-font@v3.4.0/dist/font-face.css">
+        <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css">
+        <!-- Style -->
+        <link rel="stylesheet" href="/style.css?ver=1">
+    </head>
+    <body>
+        <?php include "_inc/_header.php" ?>
+
+        <section class="normal" style="margin-bottom: 0px;">
+            <h2>اسامی مقاله های پذیرفته شده پوستری کنفرانس</h2>
+            <p>
+            </p>
+        </section>
+
+        <?php include "_inc/_footer.php" ?>
+
+        <script src="/script.js?ver=1"></script>
+    </body>
+</html>
